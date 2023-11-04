fa1fdf399427e87f964b880abc5c8b19684e5ef7
04amanrajj <04.aman.raj@gmail.com>
2023-11-04 21:04:43 +0530

    new codes

---
+++ Palindrom.js
@@ -0,0 +1,13 @@
+function isPalindrome(str){
+    console.log(str === str.split('').reverse().join('')  ? 'palindrome' : 'no palindrome');
+}
+
+
+
+let arr = [ 'aaa' ,'asdfasdf','aman','naman']
+
+
+for(let a of arr){
+   // isPalindrome(a)
+   
+}
---
+++ main.js
@@ -0,0 +1,23 @@
+let arr = [1,2,4,1,3,6]
+
+let max = 0;
+for(let i = 0;i< arr.length;i++){
+    if(max < arr[i]){
+
+        max = arr[i]
+    }
+    
+}
+
+console.log(max);
+
+
+
+
+/*
+
+max = 4
+i =5
+
+
+*/
