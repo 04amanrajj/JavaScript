6a095d1a5453f66647e99ed0e42641368a9f06de
Aman Raj <136756095+04amanrajj@users.noreply.github.com>
2024-03-05 03:11:41 +0530

    fixing

--- function/q3.js
+++ function/q3.js
@@ -1,17 +1,17 @@
-function tom(x,y){
-    return x+y;
+function tom(x, y) {
+  return x + y;
 }
-function jerry(z){
-    return z*5
+function jerry(z) {
+  return z * 5;
 }
-function bob(z){
-    return z/10
+function bob(z) {
+  return z / 10;
 }
-function oggy(z){
-    return z+10
+function oggy(z) {
+  return z + 10;
 }
-let step1=tom(10,10)
-let step2=jerry(step1)
-let step3=bob(step2)
-let step4=oggy(step3)
-console.log(step4)
+let step1 = tom(10, 10);
+let step2 = jerry(step1);
+let step3 = bob(step2);
+let step4 = oggy(step3);
+console.log(step4);
