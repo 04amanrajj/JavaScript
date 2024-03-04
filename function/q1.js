2b7c20d9f42f63d91e8d42fe11b30b4e1251428f
04amanrajj <04.aman.raj@gmail.com>
2024-03-04 16:38:24 +0530

    4 function of math

---
+++ function/q1.js
@@ -0,0 +1,21 @@
+function add(){
+    let a=1+2
+    console.log(a)
+}
+function minus(){
+    let b=5-3
+    console.log(b)
+}
+function multi(){
+    let c=4*3
+    console.log(c)
+}
+function devide(){
+    let d=6/2
+    console.log(d)
+}
+
+add();
+minus()
+multi()
+devide()
