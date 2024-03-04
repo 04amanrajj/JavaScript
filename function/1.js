d28f3fa69c2628fdf410efc8474a56b0d8ab7055
04amanrajj <04.aman.raj@gmail.com>
2024-03-04 13:13:57 +0530

    making funtions

--- function/1.js
+++ function/1.js
@@ -1,1 +1,23 @@
-console.log("hello")
+// old tv
+// let a =10
+// let b=20
+// console.log(a+b);
+
+// let x =10
+// let  y=5
+// console.log(x-y);
+// new tv
+// function diclaration
+function add(){
+    let a =10
+    let b=20
+    console.log(a+b);
+}
+
+function minus(){
+    let x =10
+let  y=5
+console.log(x-y);
+}
+
+minus();
