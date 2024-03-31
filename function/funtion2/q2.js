bf80a536f43b3835bb1861d3df96a097a773765c
Aman Raj <136756095+04amanrajj@users.noreply.github.com>
2024-03-31 23:54:33 +0530

    Update q2.js

--- function/funtion2/q2.js
+++ function/funtion2/q2.js
@@ -1,7 +1,7 @@
 // finding last and first index ofan array
 let pc = ["cpu", "ram", "gpu", "psu", "ram"];
 box = pc.lastIndexOf("ram");
-console.log(box); //output
+console.log(box); //output 
 
 box2=pc.indexOf("ram")
 console.log(box2);
